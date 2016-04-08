e0453eb71f6e0c41c2040b15fafd17312fd8d7a1
seanreed1111 <seanreed1111@gmail.com>
2016-04-08 12:14:24 -0400

    test 1-5 are now passing

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ActiveRecord::Base
+  validates :firstname, presence: true
+  validates :lastname, presence: true
+  validates :email, presence: true, uniqueness: true  
 end
--- spec/models/contact_spec.rb
+++ spec/models/contact_spec.rb
@@ -10,9 +10,41 @@ describe Contact do
       email: 'tester@test.com')
     expect(contact).to be_valid
   end
-  it "is invalid without a firstname"
-  it "is invalid without a lastname"
-  it "is invalid without a email address"
-  it "is invalid with a duplicate email address"
+
+  it "is invalid without a firstname" do
+    contact = Contact.new(
+      firstname: nil,
+      lastname: 'Sumner',
+      email: 'tester@test.com')
+    expect(contact).not_to be_valid
+  end
+
+  it "is invalid without a lastname" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: nil,
+      email: 'tester@test.com')
+    expect(contact).not_to be_valid
+  end
+  it "is invalid without a email address" do
+    contact = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: nil)
+    expect(contact).not_to be_valid
+  end
+
+  it "is invalid with a duplicate email address" do
+    contact = Contact.create(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'aaron@aaron.com')
+
+    duplicateEmail = Contact.new(
+      firstname: 'Aaron',
+      lastname: 'Sumner',
+      email: 'aaron@aaron.com')
+    expect(duplicateEmail).not_to be_valid
+  end
   it "returns a contact's full name as a string"
 end
